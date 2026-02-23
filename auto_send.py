9ecfdfcf5dcc48823deb329ec1d1da3b01b250bf
Hriday Andani <hridayandani@hridays-air.lan>
2026-02-22 23:03:00 -0800

    Print time

--- auto_send.py
+++ auto_send.py
@@ -1,18 +1,9 @@
-import asyncio
 from datetime import datetime
 import time
-from send_chunk import send_story
-
-TARGET_HOUR = 19   # change for your test
-TARGET_MINUTE = 1
 
 while True:
     now = datetime.utcnow()
-
-    if now.hour == TARGET_HOUR and now.minute == TARGET_MINUTE:
-        asyncio.run(send_story())
-        time.sleep(60)  # prevent multiple sends in same minute
-
+    print("UTC:", now)
     time.sleep(5)
 
 
