f838dbb40ddf6661450725d1b9e9c73033c16089
Hriday Andani <hridayandani@hridays-air.lan>
2026-02-22 22:28:59 -0800

    Restore auto_send

--- auto_send.py
+++ auto_send.py
@@ -3,22 +3,17 @@ from datetime import datetime
 import time
 from send_chunk import send_story
 
-TARGET_HOUR = 10   # put your PST hour here
-TARGET_MINUTE = 28 # put your PST minute here
-
-sent_today = False
+TARGET_HOUR = 18   # 10:15 PST
+TARGET_MINUTE = 31
 
 while True:
-    now = datetime.now()  # uses server time (UTC on Railway)
+    now = datetime.now()
 
-    if now.hour == TARGET_HOUR and now.minute == TARGET_MINUTE and not sent_today:
+    if now.hour == TARGET_HOUR and now.minute == TARGET_MINUTE:
         asyncio.run(send_story())
-        sent_today = True
+        time.sleep(60)  # wait 1 minute so it doesn't spam
 
-    if now.hour == 0 and now.minute == 0:
-        sent_today = False
-
-    time.sleep(30)
+    time.sleep(5)
 
 
 
