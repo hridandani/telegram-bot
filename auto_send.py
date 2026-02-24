a58b511b75488802c1e3969a0a3b7765a13263a5
Hriday Andani <hridayandani@hridays-air.lan>
2026-02-23 20:10:26 -0800

    set correct story index

--- auto_send.py
+++ auto_send.py
@@ -2,13 +2,17 @@ import time
 from datetime import datetime
 from send_chunk import send_story
 
-TARGET_HOUR = 4   # 8 AM PST
-TARGET_MINUTE = 5
+TARGET_HOUR = 16   # 8 AM PST (UTC)
+TARGET_MINUTE = 0
+
+print("Worker started")
 
 while True:
     now = datetime.utcnow()
+    print("Current UTC:", now.hour, now.minute)
 
     if now.hour == TARGET_HOUR and now.minute == TARGET_MINUTE:
+        print("TIME MATCHED — SENDING")
         send_story()
 
     time.sleep(60)
